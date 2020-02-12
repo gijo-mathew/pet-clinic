3fc683bbb3e367eb941a9c5c5cee1e48561c4716
gijo mathew <gijomathewakm@gmail.com>
2020-02-12 21:51:55 +0100

    Added interfaces.

---
+++ pet-clinic-data/src/main/java/com/spring/petclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package com.spring.petclinic.services;
+
+import com.spring.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findById(Long id);
+
+    Owner findByLastName(String lastName);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
